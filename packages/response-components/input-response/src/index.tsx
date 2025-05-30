1d807cdf61b130a9346af702d97b41699c667880
Nathan Anderson <nathan.anderson@acer.org>
2025-05-30 17:22:08 +1000

    Working on solid custom elements and slots

--- packages/response-components/input-response/src/index.tsx
+++ packages/response-components/input-response/src/index.tsx
@@ -1,14 +1,20 @@
 import {Component, ComponentProps} from 'solid-js';
+import {customElement} from 'solid-element';
 
 export type InputResponseProps = {
   children?: ComponentProps<"span">["children"];
+  one?: string;
 }
 
 export const InputResponse: Component<InputResponseProps> = (props) => {
   return (
     <div>
-      <span>{props.children}</span>
+      <span><slot name="children">{props.children}</slot></span>
+      <div>{props.one}</div>
+
       <input />
     </div>
   );
 };
+
+customElement('acer-input-response', {one: 'test'}, InputResponse);
--- packages/response-components/input-response/vite.config.ts
+++ packages/response-components/input-response/vite.config.ts
@@ -1,6 +1,15 @@
+import { dirname, resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 
 export default defineConfig({
   plugins: [solid()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.tsx'),
+      formats: ['es', 'iife'],
+      name: 'AcerInputResponse'
+    },
+    minify: false
+  }
 })
